f9e860f5980c95129376ef86072a850191a35a1a
laurazha <zhaxs820@gmail.com>
2018-03-30 23:02:18 -0700

    hw5 add mlab

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost:27017/cs5610hw');
-// var db = 'mongodb://laurazha_cs5610hw:cs5610@ds263847.mlab.com:63847/heroku_7d328xn4'
+// var db = mongoose.connect('mongodb://localhost:27017/cs5610hw');
+var db = 'mongodb://laurazha_cs5610hw:cs5610@ds263847.mlab.com:63847/heroku_7d328xn4'
 module.exports = db;
